a44ed0a7f90780830f62d7c3216a5269e9008079
Sikem Iglesias <sikemig@gmail.com>
2024-05-01 13:15:55 +0200

    el develop2 sube su paquete, clase y metodo propio

---
+++ src/sikemDevelop2/SikemDevelop2.java
@@ -0,0 +1,12 @@
+package sikemDevelop2;
+
+public class SikemDevelop2 {
+
+	public static void main(String[] args) {
+		nombreDevelop2();
+	}
+	
+	public static void nombreDevelop2() {
+		System.out.println("Hola, soy Sikem de la rama develop2");
+	}
+}
