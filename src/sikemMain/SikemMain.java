e8a9fd85a18e3a5cd1c36a35469a3705656ec6e6
Sikem Iglesias <sikemig@gmail.com>
2024-05-01 12:02:20 +0200

    SikemMain añade el gitignore y su paquete con su clase y su método particular

---
+++ src/sikemMain/SikemMain.java
@@ -0,0 +1,12 @@
+package sikemMain;
+
+public class SikemMain {
+	
+	public static void main(String[] args) {
+		nombreMain();
+	}
+	
+	public static void nombreMain() {
+		System.out.println("Hola, soy Sikem de la rama main");
+	}
+}
