0f40edd3feac3f5108d4395f7baaf7cb8e19e356
Sikem Iglesias <sikemig@gmail.com>
2024-05-01 15:39:49 +0200

    el develop1 sube la resolucion final de la clase

--- src/sikemDevelop1/SikemDevelop1.java
+++ src/sikemDevelop1/SikemDevelop1.java
@@ -4,14 +4,12 @@ public class SikemDevelop1 {
 
 	public static void main(String[] args) {
 		nombreDevelop1();
-<<<<<<< HEAD
 		
 		System.out.println("Mis comidas favoritas son:");
 		System.out.println("Tortilla de patata");
 		System.out.println("Cachopo y sidra");
 		System.out.println("Fabada asturiana");
-=======
->>>>>>> f72f075a60c27544018053e908f634baf67c20c4
+
 	}
 
 	public static void nombreDevelop1() {
