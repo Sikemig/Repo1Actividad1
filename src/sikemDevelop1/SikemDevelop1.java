f72f075a60c27544018053e908f634baf67c20c4
Sikem Iglesias <sikemig@gmail.com>
2024-05-01 14:37:04 +0200

    el develop2 corrige el conflicto en la clase develop1

--- src/sikemDevelop1/SikemDevelop1.java
+++ src/sikemDevelop1/SikemDevelop1.java
@@ -4,11 +4,6 @@ public class SikemDevelop1 {
 
 	public static void main(String[] args) {
 		nombreDevelop1();
-		
-		System.out.println("Comidas favoritas del develop2");
-		System.out.println("Sushi");
-		System.out.println("Pizza");
-		System.out.println("Pulpo a la Gallega");
 	}
 
 	public static void nombreDevelop1() {
